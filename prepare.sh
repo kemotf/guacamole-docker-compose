8c800fad30ab8c1f9d77b9f0dd4c1533e5882656
kemotf <65825381+kemotf@users.noreply.github.com>
2023-07-13 20:46:47 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -13,5 +13,5 @@ chmod -R +x ./volumes/postgres/init
 docker run --rm guacamole/guacamole /opt/guacamole/bin/initdb.sh --postgresql > ./volumes/postgres/init/initdb.sql
 echo "done"
 echo "Creating SSL certificates"
-openssl req -nodes -newkey rsa:2048 -new -x509 -keyout ./volumes/nginx/ssl/self-ssl.key -out nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.createyourown.domain/emailAddress=docker@createyourown.domain'
+openssl req -nodes -newkey rsa:2048 -new -x509 -keyout ./volumes/nginx/ssl/self-ssl.key -out ./volumes/nginx/ssl/self.cert -subj '/C=DE/ST=BY/L=Hintertupfing/O=Dorfwirt/OU=Theke/CN=www.h03.local/emailAddress=kemotf@gmail.com'
 echo "You can use your own certificates by placing the private key in ./volumes/nginx/ssl/self-ssl.key and the cert in ./volumes/nginx/ssl/self.cert"
